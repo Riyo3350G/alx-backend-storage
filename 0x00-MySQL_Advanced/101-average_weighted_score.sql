4287c62e5536d9f271f57608c9bef5abea0ee0ad
riyo3350g <yechouben@gamail.com>
2024-01-19 01:16:45 +0100

    Add Task 13 ADV

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     UPDATE users AS US,
     (SELECT U.id, SUM(score * weight) / SUM(weight) AS weighted_avrg
-    FROM users AS US
-    JOIN corrections AS C ON US.id = C.user_id
+    FROM users AS U
+    JOIN corrections AS C ON U.id = C.user_id
     JOIN projects AS P ON C.project_id = P.id
-    GROUP BY US.id) AS WAV
-    SET US.average_score = WAV.weighted_avrg
-    WHERE US.id = WAV.id;
+    GROUP BY U.id) AS WA
+    SET US.average_score = WA.weighted_avrg
+    WHERE US.id = WA.id;
 END$$
 DELIMITER ;
