785a4c27837f644d71596e2da502ce74e374c15a
riyo3350g <yechouben@gamail.com>
2024-01-23 16:26:25 +0100

    Task 12 fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,24 +2,23 @@
 """NGINX Log stats module"""
 import pymongo
 
-# Connect to MongoDB and get the collection
-client = pymongo.MongoClient()
-db = client.logs
-collection = db.nginx
 
-# Count the number of documents in the collection
-totalCollection = collection.count_documents({})
+def nginx_logs_stats(nginx_collection):
+    """function that provides some stats about Nginx logs stored in MongoDB"""
+    totalCollection = nginx_collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    counts = [nginx_collection.count_documents({"method": m}) for m in methods]
+    status = nginx_collection\
+        .count_documents({"method": "GET", "path": "/status"})
+    print(f"{totalCollection} logs")
+    print("Methods:")
+    for m, c in zip(methods, counts):
+        print(f"\tmethod {m}: {c}")
+    print(f"{status} status check")
+    return None
 
-# Count the number of documents with different methods
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-counts = [collection.count_documents({"method": m}) for m in methods]
 
-# Count the number of documents with method=GET and path=/status
-status = collection.count_documents({"method": "GET", "path": "/status"})
-
-# Print the output
-print(f"{totalCollection} logs")
-print("Methods:")
-for m, c in zip(methods, counts):
-    print(f"\tmethod {m}: {c}")
-print(f"{status} status check")
+if __name__ == "__main__":
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    nginx_logs_stats(nginx_collection)
