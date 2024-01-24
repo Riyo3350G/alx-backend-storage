b4fd53823cfb89443f3f0e049a0ccb2214470d67
riyo3350g <yechouben@gamail.com>
2024-01-24 19:44:43 +0100

    Add Task 1 Req

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Cache Module"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -17,3 +17,23 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """Get Method"""
+        data = self._redis.get(key)
+        if fn is int:
+            return self.get_int(data)
+        elif fn is str:
+            return self.get_str(data)
+        elif callable(fn):
+            return fn(data)
+        else:
+            return data
+
+    def get_int(self, data: bytes) -> int:
+        """fun that converts data from bytes to int"""
+        return int(data)
+
+    def get_str(self, data: bytes) -> str:
+        """func that converts data from bytes to str"""
+        return data.decode("utf-8")
