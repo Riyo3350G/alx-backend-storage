a93baf84c413bb8a3eca44589a7e29b4dec27c22
riyo3350g <yechouben@gamail.com>
2024-01-24 21:02:12 +0100

    Add Task 5 AdV update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,22 +3,23 @@
 import requests
 import redis
 from functools import wraps
+from typing import Callable
 
 
 redis = redis.Redis()
 
 
-def count_access(fn: callable) -> callable:
+def count_access(method: Callable) -> Callable:
     """count access decorator"""
-    @wraps(fn)
-    def wrapper(url):
+    @wraps(method)
+    def wrapper(url) -> str:
         """wrapper function for decorator"""
-        redis.incr(f"count:{url}")
-        cached_html = redis.get(f"cached:{url}")
+        redis.incr("count:{}".format(url))
+        cached_html = redis.get("cached:{}".format(url))
         if cached_html:
             return cached_html.decode('utf-8')
-        html = fn(url)
-        redis.setex(f"cached:{url}", 10, html)
+        html = method(url)
+        redis.setex("cached:{}".format(url), 10, html)
         return html
     return wrapper
 
